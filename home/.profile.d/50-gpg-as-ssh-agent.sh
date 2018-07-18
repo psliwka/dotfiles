73c063eb8a878d7260dc4b765ff41d77ae48d766
Piotr Śliwka <psliwka@opera.com>
2018-07-18 22:44:21 +0200

    Do not overwrite forwarded $SSH_AUTH_SOCK

--- home/.profile.d/50-gpg-as-ssh-agent.sh
+++ home/.profile.d/50-gpg-as-ssh-agent.sh
@@ -1,3 +1,8 @@
 is_installed gpgconf || return
 
+if [ -n "$SSH_CONNECTION" ] && [ -n "$SSH_AUTH_SOCK" ]; then
+    # Remote client wants to forward its SSH agent, let's not interfere.
+    return
+fi
+
 export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"

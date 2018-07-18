7a9ef4e2b362744679ff3c8b1c128c738c56c901
Piotr Śliwka <psliwka@opera.com>
2018-07-18 21:49:57 +0200

    Set $REQUESTS_CA_BUNDLE correctly on Mac

--- home/.profile.d/50-python-requests.sh
+++ home/.profile.d/50-python-requests.sh
@@ -1,6 +1,13 @@
 # Tell `requests` lib to use system-wide CA bundle instead of Certifi. This is
 # useful if you have some custom CA certificates installed. For details, see
 # http://docs.python-requests.org/en/stable/user/advanced/#ssl-cert-verification
-#
-# Beware! This bundle does not exist on Mac by default.
-export REQUESTS_CA_BUNDLE=/etc/ssl/certs
+
+case "$(uname)" in
+    Linux)
+        export REQUESTS_CA_BUNDLE=/etc/ssl/certs
+        ;;
+    Darwin)
+        # Does not exist on Mac by default, created by `$ brew install openssl`
+        export REQUESTS_CA_BUNDLE=/usr/local/etc/openssl/certs
+        ;;
+esac

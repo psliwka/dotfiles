2f07464afb93113b5dc3b5a293426b2282c8f4c2
Piotr Śliwka <psliwka@opera.com>
2018-08-22 23:32:00 +0200

    Tweak $PATH on Mac

--- home/.profile.d/30-python-path.sh
+++ home/.profile.d/30-python-path.sh
@@ -1,4 +1,5 @@
-is_installed python || return
-
-# This path is not covered by `20-basic-path-addons.sh` on Mac
-add_to_path "$(python -m site --user-base)/bin"
+# These paths are not covered by `20-basic-path-addons.sh` on Mac
+for python_version in "2.7" "3"; do
+    python=python$python_version
+    is_installed $python && add_to_path "$($python -m site --user-base)/bin"
+done

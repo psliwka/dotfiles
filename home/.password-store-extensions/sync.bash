65ff8af74759ab025a2b1871aab1434f3f9c3822
Piotr Śliwka <contact@psliwka.info>
2020-01-08 22:09:04 +0100

    Add pass config

---
+++ home/.password-store-extensions/sync.bash
@@ -0,0 +1,1 @@
+pass git fetch && pass git verify-commit FETCH_HEAD && pass git rebase && pass git push
---
+++ home/.profile.d/50-password-store.sh
@@ -0,0 +1,4 @@
+# https://www.passwordstore.org/
+
+export PASSWORD_STORE_ENABLE_EXTENSIONS=true
+export PASSWORD_STORE_EXTENSIONS_DIR="$HOME/.password-store-extensions"

b6d4db17504631ac6f200f651f7a7e5534f99ccf
Piotr Śliwka <contact@psliwka.info>
2020-12-25 13:59:44 +0100

    Fix luarocks path config on Mac

--- home/.profile.d/30-lua-path.sh
+++ home/.profile.d/30-lua-path.sh
@@ -1,3 +1,3 @@
 # https://github.com/luarocks/luarocks/wiki/Using-LuaRocks#scripts-installed-by-rocks-and-the-scripts-path
 is_installed luarocks || return
-add_to_path "$(luarocks path --lr-bin | cut --delimiter=: --fields=1)"
+add_to_path "$(luarocks path --lr-bin | cut -d: -f1)"

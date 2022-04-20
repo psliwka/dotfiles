65a7280acf5a8668fd1ce9be9f4638f9e4f4fce3
Piotr Śliwka <contact@psliwka.info>
2022-04-20 18:28:23 +0200

    [nvim] Use gitsigns.nvim instead of vim-gitgutter

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -106,7 +106,6 @@ if pcall(require, 'packer') then require('packer').startup(function(use)
       terminal_colors = true,
       plugins_integrations = {
         bufferline = true,
-        gitgutter = true,
         telescope = true,
       },
       pumblend = {
@@ -123,7 +122,48 @@ if pcall(require, 'packer') then require('packer').startup(function(use)
   end
 
   -- Situational awareness enhancements
-  use {'airblade/vim-gitgutter', config = [[ vim.g.gitgutter_sign_removed_above_and_below = '‾_' ]]}
+  use {'lewis6991/gitsigns.nvim', config = function()
+      require('gitsigns').setup{
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          -- Actions
+          map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+    }
+  end}
   use {'lukas-reineke/indent-blankline.nvim', config = function()
     vim.g.indentLine_char = '▏'
     vim.g.indent_blankline_show_trailing_blankline_indent = false

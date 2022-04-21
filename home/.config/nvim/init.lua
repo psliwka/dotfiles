cbb150c5701bf07c3e4fc1abe60ba3f9d5772802
Piotr Śliwka <contact@psliwka.info>
2022-04-21 17:19:02 +0200

    [nvim] Make LSP diagnostics less obtrusive
    Too many false alerts to keep them that flashy.

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -116,12 +116,11 @@ if pcall(require, 'packer') then require('packer').startup(function(use)
     }
   end}
   use 'kyazdani42/nvim-web-devicons'
-  -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#change-diagnostic-symbols-in-the-sign-column-gutter
-  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  end
+  -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#customizing-how-diagnostics-are-displayed
+  vim.diagnostic.config({
+    virtual_text = false,
+    signs = false,
+  })
 
   -- Situational awareness enhancements
   use {'lewis6991/gitsigns.nvim', config = function()

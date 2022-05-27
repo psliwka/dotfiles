dcb201c04ed714d034ce5df69b00c13f674f447d
Piotr Śliwka <contact@psliwka.info>
2022-05-27 20:20:39 +0200

    Enable kitty-fish integration

---
+++ home/.config/fish/conf.d/kitty.fish
@@ -0,0 +1,7 @@
+# https://sw.kovidgoyal.net/kitty/shell-integration/#manual-shell-integration
+
+if set -q KITTY_INSTALLATION_DIR
+    set --global KITTY_SHELL_INTEGRATION enabled
+    source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+    set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+end

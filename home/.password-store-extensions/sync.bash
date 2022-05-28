ca540d4ba70f8a306f8e732b917fa87c8d649fd6
Piotr Śliwka <contact@psliwka.info>
2022-05-28 20:27:10 +0200

    Make `pass sync` actually handle bad signatures

--- home/.password-store-extensions/sync.bash
+++ home/.password-store-extensions/sync.bash
@@ -1,1 +1,10 @@
-pass git fetch && pass git verify-commit FETCH_HEAD && pass git rebase && pass git push
+cd $PREFIX
+git fetch
+git verify-commit FETCH_HEAD || {
+	yesno "Remote signature verification failed. Proceed anyway?" && fix_signature=1
+}
+git rebase
+if [[ "$fix_signature" -eq 1 ]]; then
+	git commit --gpg-sign --allow-empty --message "Add valid signature on top of HEAD"
+fi
+git push

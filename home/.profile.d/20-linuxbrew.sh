9de4e772f701e4196f9c17e7354f558ae2600df8
Piotr Śliwka <contact@psliwka.info>
2024-01-23 18:14:45 +0100

    Add support for Homebrew on Linux

---
+++ home/.profile.d/20-linuxbrew.sh
@@ -0,0 +1,4 @@
+# https://docs.brew.sh/Homebrew-on-Linux
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"

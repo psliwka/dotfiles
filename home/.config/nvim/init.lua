eb9902fa8a16fe23ce9c659bd3f58738acbb1948
Piotr Śliwka <contact@psliwka.info>
2024-02-19 13:54:57 +0100

    [nvim] Do not use LSP formatting

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -117,9 +117,6 @@ require("lazy").setup({
 					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
 					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
 					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "<space>f", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
 				end,
 			})
 		end,

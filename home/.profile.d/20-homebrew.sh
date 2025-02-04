9858dd8e4c71741376089b9be69b35337721cc8a
Piotr Śliwka <contact@psliwka.info>
2025-02-04 14:24:49 +0100

    Fix homebrew support on Mac

--- home/.profile.d/20-homebrew.sh
+++ home/.profile.d/20-homebrew.sh
@@ -1,4 +1,5 @@
-# https://docs.brew.sh/Homebrew-on-Linux
+test -d /opt/homebrew && eval "$(/opt/homebrew/bin/brew shellenv)"
 
+# https://docs.brew.sh/Homebrew-on-Linux
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
